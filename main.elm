b6a9b31ad5e4b55d25597c20ce756cfd95195104
SophiaUngar <sungar@oberlin.edu>
2023-05-22 23:19:24 -0400

    main file

---
+++ main.elm
@@ -0,0 +1,48 @@
+import Browser
+import Html exposing (Html, Attribute, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+-- MAIN
+
+main =
+    Browser { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+type alias Model = 
+    { recipe : String
+    }
+
+
+init : Model
+init = 
+    { recipe = "" }
+
+
+
+-- UPDATE
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model = 
+    case msg of 
+        Change newRecipe ->
+            { model | recipe = newRecipe }
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model = 
+    div []
+    [ input [ placeholder "Recipe", value model.recipe, onInput Change]
+    , div [] [ text ()]] -- do something here
