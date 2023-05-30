d6ac416f2f293d303528b22eede9bb0cc9003028
SophiaUngar <sungar@oberlin.edu>
2023-05-30 09:45:53 -0400

    adding records, they don't do anything yet

--- example.elm
+++ example.elm
@@ -4,6 +4,8 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 -- example based on https://guide.elm-lang.org/architecture/text_fields.html
 
@@ -17,18 +19,26 @@ main =
 
 -- MODEL
 
+
+-- contains all ingredients and quanitities and units
+-- should this go above or below model?
+type alias IngredientsCollection = 
+    { name : List String
+    , quantity : List Float
+    , unit : List String
+    }
+
 type alias Model = 
-    { recipe : String
-    , ingredients : Array
+    { ingredients : IngredientsCollection
     , instructions : String
     }
 
 
 init : Model
 init = 
-    { recipe = "" 
-    , ingredients = []
-    , instructions = "" }
+    { ingredients = { name=[], quantity=[], unit=[] }
+    , instructions = ""
+    }
 
 
 
@@ -36,15 +46,17 @@ init =
 
 type Msg
     = Change String
+    --| Portion Float
 
 
 update : Msg -> Model -> Model
 update msg model = 
     case msg of 
         Change newRecipe ->
-            { model | recipe = newRecipe }
-        Portion size ->
-            { model | ingredients = times( size) }
+            { model | instructions = newRecipe }
+        Portion size -> 
+        --As above this is just to get it to compile
+            { model | ingredients.quantity = times( size) }
 
 
 times : Int -> Model
@@ -61,13 +73,13 @@ view model =
     div []
         [ h1 [] [ text "Welcome to our recipe assistant!"]
         , p []
-            [ text "hi!" ] 
-        , input [ placeholder "Ingredients", value model.ingredients, onInput Change][]
-        , input [ placeholder "Instructions", value model.instructions, onInput Change][]
-        , button [ onClick portions 1/3 ] [ text 1/3x ]
-        , button [ onClick portions 1/2 ] [ text 1/2x ]
-        , button [ onClick portions 2 ] [ text 2x ]
-        , button [ onClick portions 3 ] [ text 3x ]
-        , button [ onClick portions 4 ] [ text 4x ]
-        , div [] [ text (String.reverse model.recipe)] -- do something here
+            [ text "hi!" ]
+        --, input [ placeholder "Ingredients", value (String.fromFloat model.ingredients), onInput Change][]
+        --, input [ placeholder "Instructions", value model.instructions, onInput Change][]
+        --, button [ onClick (Portion (1/3)) ] [ text "1/3x" ]
+        --, button [ onClick (Portion (1/2)) ] [ text "1/2x" ]
+        --, button [ onClick (Portion 2) ] [ text "2x" ]
+        --, button [ onClick (Portion 3) ] [ text "3x" ]
+        --, button [ onClick (Portion 4) ] [ text "4x" ]
+        , div [] [ text (model.instructions)] -- do something here
         ] 
